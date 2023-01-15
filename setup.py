753a170aa0f9151b2650bf6f6e8568ff83af9041
Volker Siepmann <volker.siepmann@gmail.com>
2023-01-15 19:29:18 +0100

    convert to toml and setup.cfg way of package def.

--- setup.py
+++ setup.py
@@ -1,38 +1,35 @@
-# -*- coding: utf-8 -*-
+from setuptools import setup
 
-# stdlib modules
-import setuptools
-# from sys import path
+if __name__ == '__main__':
+    setup()
 
-# # internal modules (add to path, so that __init__.py is not executed)
-# path.append("pyasim")
-# from version import version
+# # stdlib modules
+# import setuptools
+# # from sys import path
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
 
-from src.simu._version import VERSION
+# from src.simu._version import VERSION
 
-long_description = "TODO: write this"
+# long_description = "TODO: write this"
 
-setuptools.setup(
-    name="simu",
-    version=VERSION,
-    author="Volker Siepmann",
-    author_email="volker.siepmann@gmail.com",
-    description="Equation oriented process modelling library",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/VolkerSiep/SiMu",
-    # package_data={'simu': ['units.xml']},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-    ],
-    python_requires='>=3.9',
-    install_requires=["numpy", "scipy", "casadi", "pyyaml"],
-    extras_require={
-        "dev": [
-            "Sphinx>=2.2", "sphinxcontrib-bibtex>=2.4", "pytest>=5.3",
-            "matplotlib"
-        ]
-    })
+# setuptools.setup(
+#     name="simu",
+#     version=VERSION,
+#     author="Volker Siepmann",
+#     author_email="volker.siepmann@gmail.com",
+#     description="Equation oriented process modelling library",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/VolkerSiep/SiMu",
+#     # package_data={'simu': ['units.xml']},
+#     classifiers=[
+#         "Programming Language :: Python :: 3",
+#     ],
+#     python_requires='>=3.9',
+#     install_requires=["numpy", "scipy", "casadi", "pyyaml"],
+#     extras_require={
+#         "dev": [
+#             "Sphinx>=2.2", "sphinxcontrib-bibtex>=2.4", "pytest>=5.3",
+#             "matplotlib"
+#         ]
+#     })
