ac6d7d4ecfc35c0954227b454141df45f29f4e2b
Volker Siepmann <volker.siepmann@gmail.com>
2023-08-27 15:40:25 +0200

    prepare for merge

--- unittests/model_test/model_test.py
+++ unittests/model_test/model_test.py
@@ -1,62 +1,12 @@
 """Unit tests related to the model class"""
 
 from pytest import raises
-import logging
 
-from simu.utilities import assert_reproduction, SymbolQuantity
+from simu.utilities import SymbolQuantity
 from simu.utilities.errors import DataFlowError, DimensionalityError
 # from simu.model import Augmentor, MaterialSpec
 
 from .models import *
-
-# class SquareTestModel(Model):
-#     """A simple model that calculates the square of a parameter as a surface"""
-#
-#     def interface(self):
-#         """Here I can nicely document the interface of the model"""
-#         self.parameters.define("length", 10.0, "m")
-#         self.properties.declare("area", unit="m**2")
-#
-#     def define(self):
-#         """Here I can document the internal function of the model.
-#         I can use this distinction to include this doc-string only for
-#         detailed documentations."""
-#         length = self.parameters["length"]
-#         self.properties["area"] = length * length
-#
-
-
-#
-#
-# class NestedHierarchyTestModel(Model):
-#     """A new model to test nested hierarchy. Just redefining one of the
-#     parameters, leaving the other one free."""
-#
-#     def interface(self):
-#         self.parameters.define("radius", 10.0, "cm")
-#         self.properties.declare("volume", "m**3")
-#
-#     def define(self):
-#         with self.hierarchy.add("volumator", HierarchyTestModel2()) as child:
-#             child.parameters.provide(radius=self.parameters["radius"])
-#         self.properties["volume"] = child.properties["volume"]
-#
-#
-# class MaterialTestModel(Model):
-#
-#     def interface(self):
-#         gas_spec = MaterialSpec(species=["NO", "NO2", "O2", "*"])
-#         # gas_spec.require(FancyAugmentor)
-#         self.material.require("inlet_1", gas_spec)
-#         self.material.require("inlet_2", gas_spec)
-#
-#     def define(self):
-#         inlet_1 = self.material["inlet_1"]
-#         inlet_2 = self.material["inlet_2"]
-#         # should be defined upfront for project
-#         gas: MaterialDefinition = "This is used as a template"
-#         intermediate_1 = self.material.create("intermediate_1", gas)
-#         intermediate_2 = self.material.create("intermediate_2", inlet_1.definition)
 
 
 def test_parameters_update(caplog):
@@ -119,7 +69,7 @@ def test_parameters_access():
 def test_parameters_access_too_early():
     with PropertyTestModel.proxy() as proxy:
         with raises(DataFlowError):
-            area = proxy.properties["area"]
+            _ = proxy.properties["area"]
 
 
 def test_parameters_dont_define():
@@ -134,6 +84,7 @@ def test_parameters_define_other():
     model = PropertyTestModel()
 
     def mydef():
+        """replace define"""
         model.properties["area"] = model.parameters["length"]
 
     model.define = mydef
@@ -156,56 +107,3 @@ def test_hierarchy2():
     proxy = HierarchyTestModel2.top()
     volume = proxy.properties["volume"]
     assert f"{volume:~}" == "(depth*sq((2*radius))) cm ** 3"
-
-
-
-# def test_square():
-#     """Test to instantiate the square test model and check symbols"""
-#     instance, _ = SquareTestModel.as_top_model()
-#
-#     area = instance.properties["area"]
-#     length = instance.parameters.free["length"]
-#     result = {"length": length, "area": area}
-#     assert_reproduction(result)
-#
-#
-# def test_two_instances():
-#     """Check that two instances don't interfere"""
-#     model = SquareTestModel()
-#     num = 3
-#     instances = [model.create_proxy(f"m_{i}") for i in range(num)]
-#     lengths = [SymbolQuantity(f"l_{i}", "m") for i in range(num)]
-#     for i in range(num):
-#         with instances[i] as unit:
-#             unit.parameters.provide(length=lengths[i])
-#     res = [instance.properties["area"].magnitude for instance in instances]
-#     assert_reproduction(res)
-#
-#
-# @mark.skip(reason="Didn't reimplement numerics interface yet")
-# def test_square_numerics():
-#     """Test evaluating a simple model with just parameters and properties"""
-#     instance = SquareTestModel().instance()
-#     instance.parameters.update(length="10 cm")
-#     instance.finalise()
-#
-#     numerics = instance.numerics.prepare()
-#     param = numerics.parameters
-#     assert_reproduction(param, suffix="param")
-#     #  should be something like {"length": Q("10 cm")}
-#     numerics.evaluate()  # evaluate the casadi function
-#     props = numerics.properties
-#     #  should be something like {"area": Q("100 cm**2")}
-#     assert_reproduction(props, suffix="props")
-#
-#
-
-#
-#
-# def test_hierarchy_property():
-#     """Check that calculated property has correct expression"""
-#     instance, _ = NestedHierarchyTestModel.as_top_model()
-#     vol = instance.hierarchy["volumator"]
-#     area = vol.hierarchy["square"].properties["area"].magnitude
-#     volume = instance.properties["volume"].magnitude
-#     assert_reproduction([area, volume])
